e0ac9f1c3ca7707b0a94235ccc29c0b15f837e1a
Ricky Fort <100113249+AriseSon@users.noreply.github.com>
2022-08-16 10:06:18 -0700

    AllPages

---
+++ app/javascript/components/pages/EditPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class EditPage extends Component {
+  render() {
+    return (
+      <div>Footer</div>
+    )
+  }
+}
+
+export default EditPage
---
+++ app/javascript/components/pages/IndexPage.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class IndexPage extends Component {
+  render() {
+    return (
+      <div>Footer</div>
+    )
+  }
+}
+
+export default IndexPage
