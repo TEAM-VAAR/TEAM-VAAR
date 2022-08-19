25cc13b0e12447fd6563ed54530bb0376fcb5de4
Ricky Fort <100113249+AriseSon@users.noreply.github.com>
2022-08-19 14:28:35 -0700

    Fix Indentation

--- app/javascript/components/pages/HomePage.js
+++ app/javascript/components/pages/HomePage.js
@@ -1,32 +1,23 @@
-import React, { Component } from 'react'
-import {
-  Card,
-  CardImg,
-  CardImgOverlay,
-  CardTitle,
-  CardText,
-} from "reactstrap";
+import React, { Component } from "react";
+import { Card, CardImg, CardImgOverlay, CardTitle, CardText } from "reactstrap";
 
 class HomePage extends Component {
   render() {
     return (
-      
-<div>
-  <Card inverse>
-    <CardImg
-      alt="Card image cap"
-      src="https://pbs.twimg.com/media/FaioV3DVEAAuy0l?format=jpg&name=large"
-      style={{
-        height: 900
-
-      }}
-      width="100%"
-    />
-  </Card>
-</div>
-
-    )
+      <div>
+        <Card inverse>
+          <CardImg
+            alt="Card image cap"
+            src="https://pbs.twimg.com/media/FaioV3DVEAAuy0l?format=jpg&name=large"
+            style={{
+              height: 900,
+            }}
+            width="100%"
+          />
+        </Card>
+      </div>
+    );
   }
 }
 
-export default HomePage
+export default HomePage;
