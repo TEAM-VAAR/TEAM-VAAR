c1bcb89b8fe3c6297ac4db1e1e405c3f8120a415
venessa <venessa.palmer@gmail.com>
2022-08-23 16:54:04 -0700

    offical changes

--- app/javascript/components/pages/AboutUsPage.js
+++ app/javascript/components/pages/AboutUsPage.js
@@ -11,7 +11,7 @@ class AboutUs extends Component {
   render() {
     return (
       <>
-      <h2>About Us</h2>
+        <h2>About Us</h2>
         <AboutUsCarousel />
         <div>Footer</div>
       </>
@@ -27,7 +27,9 @@ const items = [
     altText: "Project Manager",
     caption: "Alvin Accad",
     key: 1,
-    linkedin: <a href="https://www.linkedin.com/in/alvin-a-a1b826189/">linkedin</a>,
+    linkedin: (
+      <a href="https://www.linkedin.com/in/alvin-a-a1b826189/">linkedin</a>
+    ),
     GitHub: <a href=" https://github.com/alvingithub1">GitHub</a>,
     description:
       "I am a full-stack developer with experience building websites and web applications. I enjoy working with others on solving problems and communicating what can be done to solve them.",
@@ -37,7 +39,9 @@ const items = [
     altText: "Product Manager",
     caption: "Alexander Ballou",
     key: 2,
-    linkedin: <a href="https://www.linkedin.com/in/alexander-ballou/">linkedin</a>,
+    linkedin: (
+      <a href="https://www.linkedin.com/in/alexander-ballou/">linkedin</a>
+    ),
     GitHub: <a href="https://github.com/necessities">GitHub</a>,
     description:
       "I am a full stack web developer and software engineer with a background in IT and DevOps. I have experience working for a wide variety of organizations from large federal level ones to small consulting teams. I spend a lot of my free time tinkering with computers and learning about technology.",
@@ -50,14 +54,18 @@ const items = [
     linkedin: <a href="https://www.linkedin.com/in/ariseson/">linkedin</a>,
     GitHub: <a href="https://github.com/AriseSon">GitHub</a>,
     description:
-      "My name is Ricky Fort, my position in Team VAAR is the Tech Lead.",
+      "My name is Ricky Fort, my position in Team VAAR is the Tech Lead. During the process of our Honest Review Application, I displayed solid understanding of the technical needs of the project and maintained the teams highly motivated attitude.",
   },
   {
     src: "https://avatars.githubusercontent.com/u/107439475?s=400&u=1f2cea854675cd8c7bf65d929f070581aa24ace0&v=4",
     altText: "Design Lead",
     caption: "Venessa Palmer",
     key: 4,
-    linkedin: <a href="https://www.linkedin.com/in/venessa-palmer-855038225/">linkedin</a>,
+    linkedin: (
+      <a href="https://www.linkedin.com/in/venessa-palmer-855038225/">
+        linkedin
+      </a>
+    ),
     GitHub: <a href="https://github.com/VenessaSP">GitHub</a>,
     description:
       "Currently I am a student at a boot-camp style web development school in San Diego,  LEARN Academy. Here I am acquiring the necessary skills to become a junior web-developer. With over 480 hours of intense tutelage from experienced senior software developers, I am eager to apply my newly acquired skillset and be a contribution to the team! For this project I was responsible for styling, I wanted to create a visual representation of what the app is all about! I want users to focus on what the app is intended for while putting fun stylistic designs. ",
@@ -95,7 +103,6 @@ function AboutUsCarousel(args) {
         <p className="description"> {item.description}</p>
         <h3 className="linkedin"> {item.linkedin}</h3>
         <p className="GitHub"> {item.GitHub}</p>
-        
 
         <img src={item.src} alt={item.altText} />
 
